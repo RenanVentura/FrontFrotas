e189c01e301df775ac8395e11dec520c98be4dc0
Renan Ventura <renan.al.ventura@gmail.com.br>
2024-11-05 08:40:21 -0300

    Adição de Modal de notificação

--- src/Components/ModalEdit/ModalEdit.jsx
+++ src/Components/ModalEdit/ModalEdit.jsx
@@ -2,9 +2,11 @@ import './ModalEdit.css';
 import React, { useState, useEffect } from 'react';
 import Lixo from '../../assets/Lixo.png';
 import Api from '../../Services/api';
+import NotificaModal from '../ModalNotifica//ModalNotifica.jsx'
 
 const ModalEdit = ({ isOpen, onClose, onSave, onDelete = () => {}, initialData }) => {
     const [formData, setFormData] = useState(initialData || {});
+    const [isNotificaOpen, setNotificaOpen] = useState(false); 
 
     useEffect(() => {
         if (initialData) {
@@ -61,6 +63,10 @@ const ModalEdit = ({ isOpen, onClose, onSave, onDelete = () => {}, initialData }
         }
     };
 
+    const handleDeleteClick = () => {
+        setNotificaOpen(true); // Abre o modal de confirmação
+    };
+
     const handleDelete = async () => {
         try {
             const response = await Api.delete(`/solicitacao/${initialData.id}`);
@@ -75,129 +81,137 @@ const ModalEdit = ({ isOpen, onClose, onSave, onDelete = () => {}, initialData }
         } catch (error) {
             console.error("Erro ao deletar a solicitação:", error);
         }
+        setNotificaOpen(false); // Fecha o modal de confirmação após a deleção
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div className="modal-edit">
-                <div className="modal-edit-content">
-                    <div className="modal-edit-header">
-                        <h2 className="modal-edit-title">Editar Solicitação</h2>
-                        <div style={{ display: 'flex', gap: '10px' }}>
-                            <span className="modal-edit-close" onClick={onClose}>&times;</span>
-                            <img
-                                src={Lixo}
-                                alt="Lixeira"
-                                className="modal-edit-trash"
-                                onClick={handleDelete} 
-                                style={{ width: '24px', height: '24px', cursor: 'pointer' }}
-                            />
+        <>
+            <form onSubmit={handleSubmit}>
+                <div className="modal-edit">
+                    <div className="modal-edit-content">
+                        <div className="modal-edit-header">
+                            <h2 className="modal-edit-title">Editar Solicitação</h2>
+                            <div style={{ display: 'flex', gap: '10px' }}>
+                                <span className="modal-edit-close" onClick={onClose}>&times;</span>
+                                <img
+                                    src={Lixo}
+                                    alt="Lixeira"
+                                    className="modal-edit-trash"
+                                    onClick={handleDeleteClick} 
+                                    style={{ width: '24px', height: '24px', cursor: 'pointer' }}
+                                />
+                            </div>
                         </div>
+                        <label className="modal-edit-label">
+                            Status:
+                            <select 
+                                name="Estado" 
+                                className="modal-edit-input" 
+                                value={formData.Estado || ''} 
+                                onChange={handleChange} 
+                                required
+                            >
+                                <option value="Pendente">Pendente</option>
+                                <option value="Fechado">Fechado</option>
+                            </select>
+                        </label>
+                        <label className="modal-edit-label">
+                            Data Encerrado:
+                            <input
+                                type="text"
+                                name="DataEncerrado"
+                                className="modal-edit-input"
+                                value={formData.DataEncerrado || ''}
+                                onChange={handleChange}
+                                placeholder="DD/MM/YYYY"
+                            />
+                        </label>
+                        <label className="modal-edit-label">
+                            Equipamento:
+                            <input
+                                type="text"
+                                name="Equipamento"
+                                className="modal-edit-input"
+                                value={formData.Equipamento || ''}
+                                onChange={handleChange}
+                                required
+                            />
+                        </label>
+                        <label className="modal-edit-label">
+                            Solicitante:
+                            <input
+                                type="text"
+                                name="Solicitante"
+                                className="modal-edit-input"
+                                value={formData.Solicitante || ''}
+                                onChange={handleChange}
+                                required
+                            />
+                        </label>
+                        <label className="modal-edit-label">
+                            Filial:
+                            <input
+                                type="text"
+                                name="Filial"
+                                className="modal-edit-input"
+                                value={formData.Filial || ''}
+                                onChange={handleChange}
+                                required
+                            />
+                        </label>
+                        <label className="modal-edit-label">
+                            Tipo de Serviço:
+                            <input
+                                type="text"
+                                name="TipoServ"
+                                className="modal-edit-input"
+                                value={formData.TipoServ || ''}
+                                onChange={handleChange}
+                                required
+                            />
+                        </label>
+                        <label className="modal-edit-label">
+                            Serviço/Item:
+                            <input
+                                type="text"
+                                name="Servico"
+                                className="modal-edit-input"
+                                value={formData.Servico || ''}
+                                onChange={handleChange}
+                                required
+                            />
+                        </label>
+                        <label className="modal-edit-label">
+                            Urgência:
+                            <input
+                                type="text"
+                                name="Urgencia"
+                                className="modal-edit-input"
+                                value={formData.Urgencia || ''}
+                                onChange={handleChange}
+                                required
+                            />
+                        </label>
+                        <label className="modal-edit-label">
+                            Descrição:
+                            <textarea
+                                name="Descricao"
+                                className="modal-edit-textarea"
+                                value={formData.Descricao || ''}
+                                onChange={handleChange}
+                                required
+                            />
+                        </label>
+                        <button type="submit" className="modal-edit-button">Salvar</button>
                     </div>
-                    <label className="modal-edit-label">
-                        Status:
-                        <select 
-                            name="Estado" 
-                            className="modal-edit-input" 
-                            value={formData.Estado || ''} 
-                            onChange={handleChange} 
-                            required
-                        >
-                            <option value="Pendente">Pendente</option>
-                            <option value="Fechado">Fechado</option>
-                        </select>
-                    </label>
-                    <label className="modal-edit-label">
-                        Data Encerrado:
-                        <input
-                            type="text"
-                            name="DataEncerrado"
-                            className="modal-edit-input"
-                            value={formData.DataEncerrado || ''}
-                            onChange={handleChange}
-                            placeholder="DD/MM/YYYY"
-                        />
-                    </label>
-                    <label className="modal-edit-label">
-                        Equipamento:
-                        <input
-                            type="text"
-                            name="Equipamento"
-                            className="modal-edit-input"
-                            value={formData.Equipamento || ''}
-                            onChange={handleChange}
-                            required
-                        />
-                    </label>
-                    <label className="modal-edit-label">
-                        Solicitante:
-                        <input
-                            type="text"
-                            name="Solicitante"
-                            className="modal-edit-input"
-                            value={formData.Solicitante || ''}
-                            onChange={handleChange}
-                            required
-                        />
-                    </label>
-                    <label className="modal-edit-label">
-                        Filial:
-                        <input
-                            type="text"
-                            name="Filial"
-                            className="modal-edit-input"
-                            value={formData.Filial || ''}
-                            onChange={handleChange}
-                            required
-                        />
-                    </label>
-                    <label className="modal-edit-label">
-                        Tipo de Serviço:
-                        <input
-                            type="text"
-                            name="TipoServ"
-                            className="modal-edit-input"
-                            value={formData.TipoServ || ''}
-                            onChange={handleChange}
-                            required
-                        />
-                    </label>
-                    <label className="modal-edit-label">
-                        Serviço/Item:
-                        <input
-                            type="text"
-                            name="Servico"
-                            className="modal-edit-input"
-                            value={formData.Servico || ''}
-                            onChange={handleChange}
-                            required
-                        />
-                    </label>
-                    <label className="modal-edit-label">
-                        Urgência:
-                        <input
-                            type="text"
-                            name="Urgencia"
-                            className="modal-edit-input"
-                            value={formData.Urgencia || ''}
-                            onChange={handleChange}
-                            required
-                        />
-                    </label>
-                    <label className="modal-edit-label">
-                        Descrição:
-                        <textarea
-                            name="Descricao"
-                            className="modal-edit-textarea"
-                            value={formData.Descricao || ''}
-                            onChange={handleChange}
-                            required
-                        />
-                    </label>
-                    <button type="submit" className="modal-edit-button">Salvar</button>
                 </div>
-            </div>
-        </form>
+            </form>
+            <NotificaModal 
+                isOpen={isNotificaOpen} 
+                onClose={() => setNotificaOpen(false)} 
+                onConfirm={handleDelete} 
+            />
+        </>
     );
 };
 
--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -4,12 +4,15 @@ import api from '../../Services/api';
 import Logo from '../../assets/Logo Qually-Sem fundo LetraPreta.png';
 import icon from '../../assets/lapis.png';
 import ModalEdit from '../../Components/ModalEdit/ModalEdit.jsx';
+import ModalNotifica from '../../Components/ModalNotifica/ModalNotifica.jsx'; 
 import Lixo from '../../assets/lixo.png';
 
 function ListaSolicitacao() {
     const [solicitacao, setSolicitacao] = useState([]);
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isNotificaOpen, setIsNotificaOpen] = useState(false); 
     const [selectedData, setSelectedData] = useState(null);
+    const [deleteId, setDeleteId] = useState(null); 
 
     async function getSolicitacao() {
         try {
@@ -30,14 +33,23 @@ function ListaSolicitacao() {
         setIsModalOpen(true);
     };
 
-    const handleDelete = async (id) => {
-        try {
-            await api.delete(`/solicitacao/${id}`); 
-            setSolicitacao(prevSolicitacao => prevSolicitacao.filter(item => item.id !== id));
-            console.log("Solicitação deletada:", id);
-        } catch (error) {
-            console.error("Erro ao deletar solicitação:", error);
+    const handleDeleteClick = (id) => {
+        setDeleteId(id); // Guarda o ID que será deletado
+        setIsNotificaOpen(true); // Abre o modal de notificação
+    };
+
+    const handleDelete = async () => {
+        if (deleteId) {
+            try {
+                await api.delete(`/solicitacao/${deleteId}`); 
+                setSolicitacao(prevSolicitacao => prevSolicitacao.filter(item => item.id !== deleteId));
+                console.log("Solicitação deletada:", deleteId);
+                setDeleteId(null); // Limpa o ID
+            } catch (error) {
+                console.error("Erro ao deletar solicitação:", error);
+            }
         }
+        setIsNotificaOpen(false); // Fecha o modal de notificação
     };
 
     const handleSave = (updatedData) => {
@@ -59,7 +71,7 @@ function ListaSolicitacao() {
                                 <button className='Edit' onClick={() => handleEditClick(dados)}>
                                     <img src={icon} alt="edit" className='Logo' />
                                 </button>
-                                <button className='Delete' onClick={() => handleDelete(dados.id)}>
+                                <button className='Delete' onClick={() => handleDeleteClick(dados.id)}>
                                     <img src={Lixo} alt="delete" className='Logo' />
                                 </button>
                             </div>
@@ -85,6 +97,13 @@ function ListaSolicitacao() {
                     onClose={() => setIsModalOpen(false)}
                     onSave={handleSave}
                     initialData={selectedData}
+                />
+            )}
+            {isNotificaOpen && (
+                <ModalNotifica
+                    isOpen={isNotificaOpen}
+                    onClose={() => setIsNotificaOpen(false)}
+                    onConfirm={handleDelete}
                 />
             )}
         </div>
